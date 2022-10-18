f7ce60cfcc315c588f21ad757b6357c115f91463
Oluwasegun Olawale Benjamin <benjaminoluwasegun@gmail.com>
2022-10-18 15:08:43 -0700

    Completed Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - a program that prints _putchar
- * return 0, if successful
+ * main -  prints _putchar, followed by a new line
+ * Return 0, if successful
  */
-int main()
+
+int main (void)
 {
-	printf("_putchar\n");
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	
 	return (0);
 }
