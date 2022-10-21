6bf5c0239f613cddfea77def5af6224e7f0f180a
Oluwasegun Olawale Benjamin <benjaminoluwasegun@gmail.com>
2022-10-20 17:46:57 -0700

    Completed Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @b: int to be multiplied to a
+ * @a: int to be multiplied to b
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
