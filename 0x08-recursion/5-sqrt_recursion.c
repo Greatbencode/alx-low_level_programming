442cbfd1caa0f8b76df75ac74de959f9bae88b1d
Oluwasegun Olawale Benjamin <benjaminoluwasegun@gmail.com>
2022-11-02 14:08:54 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,51 @@
 #include "main.h"
 
+int sqrt_rec_odd(int start, int n);
 /**
- * _sqrt_recursion - function that returns the natural
- * square root of a number
- * @n: input number
- * Return: int
+ * _sqrt_recursion - find the square root of a given number.
+ * @n: number to find the square root of.
+ *
+ * Return: the sqrt as a Int.
  */
-
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
+	int sqrt;
+
 	if (n < 0)
 		return (-1);
-	return (getNumSqr(n, 1));
+	else if (n <= 1 && n >= 0)
+		return (n);
+
+	if (!(n % 2))
+	{
+		if (n / 2 == 2 || n == 2)
+			return (2);
+		sqrt = _sqrt_recursion(n / 2);
+		if (n / sqrt == sqrt)
+			return (sqrt);
+		else if (sqrt > 1)
+			return (sqrt * 2);
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_rec_odd(n, n - 2));
+	}
 }
 
 /**
- * getNumSqr - function that gets de num of sqroot
- * @num: input number
- * @i: Counter variable
- * Return: int
+ * sqrt_rec_odd - helper to deal with odd numbers and primes.
+ * @start: the odd or prime number to check for sqrt.
+ * @n: number to find the square root of.
+ *
+ * Return: the sqrt as a Int.
  */
-
-int getNumSqr(int num, int i)
+int sqrt_rec_odd(int start, int n)
 {
-	if (num == (i * i))
-		return (i);
-	else if (num > (i * i))
-		return (getNumSqr(num, i + 1));
-	else
+	if (n < 0)
 		return (-1);
+	else if (start == n * n)
+		return (n);
+	else
+		return (sqrt_rec_odd(start, n - 2));
 }
