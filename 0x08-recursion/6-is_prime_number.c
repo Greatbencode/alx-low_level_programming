70809e346ae4d1bfc2a0dc2533151051c1015622
Oluwasegun Olawale Benjamin <benjaminoluwasegun@gmail.com>
2022-11-02 13:55:30 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,25 +7,35 @@
  * Return: int
  */
 
+int prime_number(int n, int i);
+
+/**
+ * is_prime_number - fuction the check if a number is a prime.
+ * @n: number to be tested.
+ *
+ * Return: 1 if a Prime else 0
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (getNumPrim(n, 2));
+	return (prime_number(n, n - 1));
 }
 
 /**
- * getNumPrim - function that gets if num is 0 or 1
- * @num: input number
- * @i: Counter variable
- * Return: int
+ * prime_number - help fuction function for is_prime_number.
+ * @n: Original number to be tested.
+ * @i: Original number to test with.
+ *
+ * Return: 1 if a Prime else 0
  */
-
-int getNumPrim(int num, int i)
+int prime_number(int n, int i)
 {
-	if (num == i)
+	if (i == 1)
 		return (1);
-	if (num % i == 0)
+	else if (n % i == 0)
 		return (0);
-	return (getNumPrim(num, i + 1));
+	else if (n % i != 0)
+		return (prime_number(n, i - 1));
+	return (0);
 }
